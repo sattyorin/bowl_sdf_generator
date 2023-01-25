651ca6506ae8c9da34066ee8b114bf2c97f3768a
sattyorin <sattyorin.sattyon@gmail.com>
2023-01-26 06:06:02 +0900

    added output directory for mujoco

--- make_bowl.py
+++ make_bowl.py
@@ -10,6 +10,7 @@ from stl import mesh
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--model_name", default="bowl", type=str, help="name of model")
     parser.add_argument(
         "-n", "--num_division", default=16, type=int, help="number of division"
     )
@@ -20,17 +21,22 @@ def parse_args() -> argparse.Namespace:
         "--radius_top", type=float, required=True, help="radius on bottom"
     )
     parser.add_argument(
-        "--height", type=float, required=True, help="height of the cooking_bowl"
+        "--height", type=float, required=True, help="height of the bowl"
     )
     parser.add_argument(
-        "--thickness", type=float, required=True, help="thickness of the cooking_bowl"
+        "--thickness", type=float, required=True, help="thickness of the bowl"
     )
     parser.add_argument(
-        "-o",
-        "--out_dir",
+        "--out_mujoco_dir",
         default=".",
         required=True,
-        help="thickness of the cooking_bowl",
+        help="output mujoco directory",
+    )
+    parser.add_argument(
+        "--out_gazebo_dir",
+        default=".",
+        required=True,
+        help="output gazebo directory",
     )
 
     return parser.parse_args()
@@ -40,22 +46,26 @@ class BowlGenerator:
     def __init__(
         self,
         *,
+        model_name: str,
         num_division: int,
         radius_bottom: float,
         radius_top: float,
         height: float,
         thickness: float,
-        out_dir: str,
+        out_mujoco_dir: str,
+        out_gazebo_dir: str,
     ):
+        self.model_name = model_name
         self.N = num_division
         self.radius_bottom = radius_bottom
         self.radius_top = radius_top
         self.height = height
         self.thickness = thickness
-        self.out_dir = out_dir
+        self.out_mujoco_dir = out_mujoco_dir
+        self.out_gazebo_dir = out_gazebo_dir
 
-        os.makedirs(self.out_dir, exist_ok=True)
-        os.makedirs(os.path.join(self.out_dir, "meshes"), exist_ok=True)
+        os.makedirs(self.out_gazebo_dir, exist_ok=True)
+        os.makedirs(os.path.join(self.out_gazebo_dir, "meshes"), exist_ok=True)
 
     def gen_bottom_plate_stl(self):
         theta = np.hstack([np.arange(0, 2.0 * np.pi, 2.0 * np.pi / self.N), 0.0])
@@ -125,7 +135,12 @@ class BowlGenerator:
             for j in range(3):
                 model_mesh.vectors[i][j] = vertices[f[j], :]
 
-        model_mesh.save(os.path.join(self.out_dir, "meshes", "cooking_bowl_bottom.stl"))
+        model_mesh.save(
+            os.path.join(self.out_mujoco_dir, "meshes", f"{self.model_name}_bottom.stl")
+        )
+        model_mesh.save(
+            os.path.join(self.out_gazebo_dir, "meshes", f"{self.model_name}_bottom.stl")
+        )
 
     def gen_side_fragment_stl(self):
         theta_width = 2.0 * np.pi / self.N
@@ -183,18 +198,25 @@ class BowlGenerator:
                 model_mesh.vectors[i][j] = vertices[f[j], :]
 
         model_mesh.save(
-            os.path.join(self.out_dir, "meshes", "cooking_bowl_fragment.stl")
+            os.path.join(
+                self.out_mujoco_dir, "meshes", f"{self.model_name}_fragment.stl"
+            )
+        )
+        model_mesh.save(
+            os.path.join(
+                self.out_gazebo_dir, "meshes", f"{self.model_name}_fragment.stl"
+            )
         )
 
     def gen_model_config(self):
-        CONTENTS = """<?xml version = "1.0"?>
+        CONTENTS = f"""<?xml version = "1.0"?>
 <model>
-  <name>cooking_bowl</name>
+  <name>{self.model_name}</name>
   <version>1.0</version>
   <sdf version="1.7">model.sdf</sdf>
 </model>
 """
-        with open(os.path.join(self.out_dir, "model.config"), mode="w") as f:
+        with open(os.path.join(self.out_gazebo_dir, "model.config"), mode="w") as f:
             f.write(CONTENTS)
 
     def gen_model_sdf(self):
@@ -209,7 +231,7 @@ class BowlGenerator:
                     f"       <pose>0 0 0 0 0 {theta}</pose>",
                     "        <geometry>",
                     "          <mesh>",
-                    "            <uri>model://cooking_bowl/meshes/cooking_bowl_fragment.stl</uri>",
+                    f"            <uri>model://{self.model_name}/meshes/{self.model_name}_fragment.stl</uri>",
                     "          </mesh>",
                     "        </geometry>",
                     "      </collision>",
@@ -217,7 +239,7 @@ class BowlGenerator:
                     f"       <pose>0 0 0 0 0 {theta}</pose>",
                     "        <geometry>",
                     "          <mesh>",
-                    "            <uri>model://cooking_bowl/meshes/cooking_bowl_fragment.stl</uri>",
+                    f"            <uri>model://{self.model_name}/meshes/{self.model_name}_fragment.stl</uri>",
                     "          </mesh>",
                     "        </geometry>",
                     "      </visual>",
@@ -230,7 +252,7 @@ class BowlGenerator:
         lines = [
             '<?xml version="1.0" ?>',
             '<sdf version="1.7">',
-            '  <model name="cooking_bowl">',
+            f'  <model name="{self.model_name}">',
             "    <pose>0 0 0 0 0 0</pose>",
             "    <static>true</static>",
             '    <link name="bottom_plate">',
@@ -240,14 +262,14 @@ class BowlGenerator:
             '      <collision name="collision">',
             "        <geometry>",
             "          <mesh>",
-            "            <uri>model://cooking_bowl/meshes/cooking_bowl_bottom.stl</uri>",
+            f"            <uri>model://{self.model_name}/meshes/{self.model_name}_bottom.stl</uri>",
             "          </mesh>",
             "        </geometry>",
             "      </collision>",
             '      <visual name="visual">',
             "        <geometry>",
             "          <mesh>",
-            "            <uri>model://cooking_bowl/meshes/cooking_bowl_bottom.stl</uri>",
+            f"            <uri>model://{self.model_name}/meshes/{self.model_name}_bottom.stl</uri>",
             "          </mesh>",
             "        </geometry>",
             "      </visual>",
@@ -256,7 +278,33 @@ class BowlGenerator:
             "  </model>" "</sdf>",
         ]
 
-        with open(os.path.join(self.out_dir, "model.sdf"), mode="w") as f:
+        with open(os.path.join(self.out_gazebo_dir, "model.sdf"), mode="w") as f:
+            f.write("\n".join(lines) + "\n")
+
+    def gen_model_mujoco_xml(self):
+        fragments = [
+            "\n".join(
+                [
+                    f'    <geom name="geom_bowl_fragment{i}" type="mesh" mesh="{self.model_name}_fragment" pos="0 0 0" euler="0 0 {np.rad2deg(theta)}" />',
+                ]
+            )
+            for i, theta in enumerate(np.arange(0.0, 2.0 * np.pi, 2.0 * np.pi / self.N))
+        ]
+
+        lines = [
+            '<?xml version="1.0"?>',
+            "<mujoco>",
+            '  <body name="bowl" pos="0 0 0.1">',
+            f'    <!-- <geom name="geom_bowl_bottom0" type="mesh" mesh="{self.model_name}_bottom" pos="0 0 0" euler="0 0 0" /> -->',
+            f'    <geom name="geom_bowl_bottom0" type="ellipsoid" size="{self.radius_bottom} {self.radius_bottom} 0.01" pos="0 0 0" />',
+            *fragments,
+            "  </body>",
+            "</mujoco>",
+        ]
+
+        with open(
+            os.path.join(self.out_mujoco_dir, "xmls", "stir_bowl.xml"), mode="w"
+        ) as f:
             f.write("\n".join(lines) + "\n")
 
 
@@ -264,18 +312,21 @@ def main():
     args = parse_args()
 
     generator = BowlGenerator(
+        model_name=args.model_name,
         num_division=args.num_division,
         radius_bottom=args.radius_bottom,
         radius_top=args.radius_top,
         height=args.height,
         thickness=args.thickness,
-        out_dir=args.out_dir,
+        out_mujoco_dir=args.out_mujoco_dir,
+        out_gazebo_dir=args.out_gazebo_dir,
     )
 
     generator.gen_side_fragment_stl()
     generator.gen_bottom_plate_stl()
     generator.gen_model_config()
     generator.gen_model_sdf()
+    generator.gen_model_mujoco_xml()
 
 
 if __name__ == "__main__":
--- sample_usage.sh
+++ sample_usage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./make_bowl.py --num_division 16 --radius_bottom 0.08 --radius_top 0.12 --height 0.06 --thickness 0.001 --out_dir ~/.gazebo/models/cooking_bowl
+./make_bowl.py --model_name cooking_bowl --num_division 16 --radius_bottom 0.08 --radius_top 0.12 --height 0.06 --thickness 0.001 --out_mujoco_dir ~/lab/reinforcement_learning/envs/stir --out_gazebo_dir ~/lab/ros/stir_description/models
